953fff04ea4686e277cb5584a14165e5fece5bf9
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-10 15:05:23 +0800

    (Java Exam) - Add implementations

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,20 @@
 package com.tw;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public int add(String string) {
-        return 0;
+        String[] processedString = Arrays.stream(string.split("[/;\n,;-|]"))
+                .filter(s -> !s.equals(""))
+                .toArray(String[]::new);
+
+        int result = 0;
+        for (String str : processedString) {
+            int intValue = Integer.parseInt(str);
+            result += intValue;
+        }
+
+
+        return result;
     }
 }
